8d89b7a3b9a5d26a503a3d32552ca4d1302e3511
WW930\a869156 <ckronberg@andovercos.com>
2023-06-26 14:21:47 -0500

    changed verified to be false before checking

--- webAuto.py
+++ webAuto.py
@@ -157,6 +157,7 @@ def verify_address(city,state,address1,address2=None):
 #Function for making the GUI
 def make_window():
     global user_name,date_chosen,env_used,state_chosen,producer_selected,create_type,browser_chosen,line_of_business,user_chosen,verified
+    verified = False
     sg.theme(THEME)
     userList = []
     browsers = ["Chrome","Edge"]
