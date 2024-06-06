cacd6f19c0e8c76290d5c141be0c8d2feb0830ea
ChrisKronbergADVR <120741240+ChrisKronbergADVR@users.noreply.github.com>
2024-06-05 21:40:53 -0500

    Update MultiLog.py

--- SupportFiles/MultiLog.py
+++ SupportFiles/MultiLog.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 class MultiLog:
 
-    filePath = "AndoverAutomation\\Logs\\"
+    filePath = "Logs/"
+    logger = None
 
     def createLog(self,state_chosen,line_of_business,log_name):
         log = logging.getLogger(log_name)
@@ -11,20 +12,21 @@ class MultiLog:
         file_formatter = logging.FileHandler(filename=f"{self.filePath}Automation_{state_chosen}_{line_of_business}_created_{time_stamp}.log") #,format='%(asctime)s - %(levelname)s - %(message)s',datefmt="%m/%d/%Y %I:%M:%S %p")
         log.setLevel(level=logging.INFO)
         log.addHandler(file_formatter)
-        return log
+        self.logger = log
 
-    def add_log(self,log,message,level):
-        if log is not None:
+    def add_log(self,message,level):
+        Log = self.logger
+        if Log is not None:
             time_stamp = datetime.now().strftime("%m-%d-%Y - %I:%M:%S %p")
             if level == logging.INFO:
-                log.info(f"{time_stamp} - INFO - {message}")
+                Log.info(f"{time_stamp} - INFO - {message}")
             if level == logging.DEBUG:
-                log.info(f"{time_stamp} - DEBUG - {message}")
+                Log.info(f"{time_stamp} - DEBUG - {message}")
             if level == logging.WARNING:
-                log.info(f"{time_stamp} - WARNING - {message}")
+                Log.info(f"{time_stamp} - WARNING - {message}")
             if level == logging.ERROR:
-                log.info(f"{time_stamp} - ERROR - {message}")
+                Log.info(f"{time_stamp} - ERROR - {message}")
             if level == logging.FATAL:
-                log.info(f"{time_stamp} - FATAL - {message}")
+                Log.info(f"{time_stamp} - FATAL - {message}")
             if level == logging.CRITICAL:
-                log.info(f"{time_stamp} - CRITICAL - {message}")
+                Log.info(f"{time_stamp} - CRITICAL - {message}")
